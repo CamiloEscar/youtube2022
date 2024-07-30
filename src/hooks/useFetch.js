2f4eee4d8cc1b51b7973e7ebcfac905248f55cf1
CamiloEscar95 <camiloescar1995@gmail.com>
2024-07-29 21:38:00 -0300

    back y front

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [
+    //si quiero que se refresque automaticamente marco url sino uso el boton para que lo haga de forma manual
+    // url
+  ]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+  return { data, loading, error, reFetch };
+};
+
+export default useFetch;
